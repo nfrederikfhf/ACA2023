72110bf08363f0915d9a2bb558ea066ddef74ea8
William Mathiassen <s194050@dtu.dk>
2023-03-18 17:18:58 +0100

    second iteration of instruction memory and topfile

---
+++ chisel-template-clean/src/main/scala/ChiselRISC.scala
@@ -0,0 +1,16 @@
+import chisel3._
+import chisel3.util._
+
+class ChiselRISC extends Module{
+  // Pipeline stages
+  //val ProgramCounter = Module(new ProgramCounter())
+  val InstructionMemory = Module(new InstructionMemory(1024, 32))
+  /* TODO: Add the rest of the pipeline stages */
+
+  // Connect the pipeline stages
+  //InstructionMemory.io.rdAdd <> ProgramCounter.io.rdAdd
+  /* TODO: Connect the rest of the pipeline stages */
+
+
+
+}
