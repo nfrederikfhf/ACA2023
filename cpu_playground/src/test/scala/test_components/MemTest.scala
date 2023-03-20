ccf11f0629880de2cfd850cba8e775a682b133ed
Niels <niels.ffh.frandsen@gmail.com>
2023-03-20 22:50:17 +0100

    register file and programcounter, untested since environment is silly

--- cpu_playground/src/test/scala/test_components/MemTest.scala
+++ cpu_playground/src/test/scala/test_components/MemTest.scala
@@ -1,17 +1,17 @@
 package test_components
-
 import chisel3._
 import chiseltest._
 import components.DualReadMem
 import org.scalatest.flatspec.AnyFlatSpec
-
 class MemTest extends AnyFlatSpec with ChiselScalatestTester {
   it should "check if address 0x0000 0000 is always 0" in {
     test(new DualReadMem(32, 32)) { dut =>
       dut.io.wrAddr.poke(0.U)
+      dut.io.wren.poke(true)
       dut.io.wrData.poke("h_dead_beef".U)
       dut.clock.step(1)
       dut.io.rdAddr1.poke(0.U)
+      dut.io.rden.poke(true)
       dut.clock.step(1)
       dut.io.rdData1.expect(0.U)
     }
