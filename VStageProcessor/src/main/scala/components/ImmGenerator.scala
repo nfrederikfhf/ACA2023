b7983bfec185782023d448fc45b582864c14d3ca
William <s194050@student.dtu.dk>
2023-03-22 14:27:47 +0100

    Fixed InstEnum.scala, by making the classes objects as it is chisel ;) Further work on the ImmediateGen as well

--- VStageProcessor/src/main/scala/components/ImmGenerator.scala
+++ VStageProcessor/src/main/scala/components/ImmGenerator.scala
@@ -2,26 +2,40 @@ package components
 import chisel3._
 import chisel3.util._
 
-
 class ImmGenerator(dataWidth: Int) extends Module{
   val io = IO(new Bundle{
-    val imm = Input(SInt(dataWidth.W))
+    val imm = Input(UInt(dataWidth.W))
     val immediate = Output(SInt(dataWidth.W))
   })
   // Seperate the instruction into different parts corresponding to the RISC-V 32I ISA and sign-extend
-  val opcode = io.imm(6,0)
-  val funct3 = io.imm(14,12)
-  val funct7 = io.imm(31,25)
+  val (opcode,_) = OP.safe(io.imm(6,0))
+  //val funct3 = io.imm(14,12)
+  //val funct7 = io.imm(31,25)
   val immI = Cat(Fill(20, io.imm(31)),io.imm(31,20)).asSInt
   val immS = Cat(Fill(20, io.imm(31)),io.imm(31,25),io.imm(11,7)).asSInt
   val immB = Cat(Fill(19,io.imm(31)),io.imm(7),io.imm(30,25),io.imm(11,8), 0.U).asSInt
   val immU = Cat(io.imm(31,12), Fill(12, 0.U)).asSInt
   val immJ = Cat(Fill(11,io.imm(31)),io.imm(19,12),io.imm(20),io.imm(30,21),0.U).asSInt
 
-  io.immediate := immI
+  val imm = WireDefault(immI) // Default immediate value is immI
 
-  
+  switch(opcode){ // Select the immediate value based on the opcode
+    is(OP.LUI){
+      imm := immU
+    }
+    is(OP.AUIPC){
+      imm := immU
+    }
+    is(OP.JAL){
+      imm := immJ
+    }
+    is(OP.ST){
+      imm:= immS
+    }
+    is(OP.BR){
+      imm := immB
+    }
+  }
 
-
-
+  io.immediate := imm
 }
--- VStageProcessor/src/main/scala/components/InstEnum.scala
+++ VStageProcessor/src/main/scala/components/InstEnum.scala
@@ -2,19 +2,19 @@ package components
 import chisel3._
 import chisel3.experimental.ChiselEnum
 
-class OP extends ChiselEnum {
+object OP extends ChiselEnum {
   val LD = Value("b0000011".U)
-  val ST = Value("b0100011".U)
-  val AR = Value("b0110011".U)
-  val BR = Value("b1100011".U)
   val IM = Value("b0010011".U)
-  val LUI = Value("b0110111".U)
   val AUIPC = Value("b0010111".U)
-  val JAL = Value("b1101111".U)
+  val ST = Value("b0100011".U)
+  val LUI = Value("b0110111".U)
+  //val AR = Value("b0110011".U)
+  val BR = Value("b1100011".U)
   val JALR = Value("b1100111".U)
+  val JAL = Value("b1101111".U)
 }
 
-class funct3 extends ChiselEnum {
+object funct3 extends ChiselEnum {
   val JALR, BEQ, LB, SB, ADDI, ADD, SUB = Value("b000".U)
   val BNE, LH, SH, SLLI, SLL = Value("b001".U)
   val LW, SW, SLTI, SLT = Value("b010".U)
@@ -25,7 +25,7 @@ class funct3 extends ChiselEnum {
   val BGEU, ANDI, AND = Value("b111".U)
 }
 
-class funct7 extends ChiselEnum {
+object funct7 extends ChiselEnum {
   val SLLI, SRLI, ADD, SLL, SLT, SLTU, XOR, SRL, AND, OR = Value("b0000000".U)
   val SRAI, SUB, SRA = Value("b0100000".U)
 
