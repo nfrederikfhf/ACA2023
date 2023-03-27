f6a8388b0c11dbba87ab7586ec321178e5b37b36
Niels <niels.ffh.frandsen@gmail.com>
2023-03-27 22:24:44 +0200

    basic decoder/controller is in place

--- VStageProcessor/src/main/scala/components/Decoder.scala
+++ VStageProcessor/src/main/scala/components/Decoder.scala
@@ -1,8 +1,8 @@
 package components
 import chisel3._
+import chisel3.util._
+import utilities.{OP, Funct3, Funct7}
 import _root_.utilities._
-
-
 
 class Decoder(datawidth: Int, addrWidth: Int)  extends Module {
   val io = IO(new Bundle {
@@ -23,29 +23,84 @@ class Decoder(datawidth: Int, addrWidth: Int)  extends Module {
   io.out.ctrl.store := WireInit(false.B)
   io.out.ctrl.addToPC := WireInit(false.B)
 
-//  switch(io.in.inst(6, 0)) {
-//    is("0110011".U) {
-//      switch(io.in.inst(30)) {
-//
-//        is("1".U) {
-//          switch(io.in.inst(14, 12)) {
-//            is("000".U) {
-//
-//            }
-//          }
-//        }
-//        is("0".U) {
-//          switch(io.in.inst(14, 12)) {
-//            is("000".U) {
-//
-//            }
-//
-//          }
-//
-//        }
-//      }
-//
-//
-//    }
-//  }
+  val (op,_) = OP.safe(io.in.inst(6,0))
+  val (funct3,_) = Funct3.safe(io.in.inst(14,12))
+  val (funct7,_) = Funct7.safe(io.in.inst(31,25))
+  switch(op) {
+    is(OP.LD) {
+      io.out.ctrl.load := true.B
+      io.out.ctrl.useImm := true.B
+    }
+
+    is(OP.IM){
+      io.out.ctrl.useALU := true.B
+      io.out.ctrl.useImm := true.B
+      switch(funct3){
+        is(Funct3.ADDI){}
+        is(Funct3.SLLI){}
+        is(Funct3.SLTI){}
+        is(Funct3.SLTIU){}
+        is(Funct3.ORI){}
+        is(Funct3.ANDI){}
+        is(Funct3.SRI){
+          switch(funct7){
+            is(Funct7.SRAI){}
+            is(Funct7.SRLI){}
+          }
+        }
+        is(Funct3.XORI){}
+      }
+    }
+
+    is(OP.AUIPC){
+      io.out.ctrl.load := true.B
+      io.out.ctrl.useImm := true.B
+    }
+
+    is(OP.ST){
+      io.out.ctrl.store := true.B
+      io.out.ctrl.useImm := true.B
+    }
+
+    is(OP.AR){
+      io.out.ctrl.useALU := true.B
+      switch(funct3){
+        is(Funct3.ADDSUB){
+          switch(funct7){
+            is(Funct7.ADD){}
+            is(Funct7.SUB){}
+          }
+        }
+        is(Funct3.SLL){}
+        is(Funct3.SLT){}
+        is(Funct3.SLTU){}
+        is(Funct3.XOR){}
+        is(Funct3.SR){
+          switch(funct7){
+            is(Funct7.SRA){}
+            is(Funct7.SRL){}
+          }
+        }
+        is(Funct3.OR){}
+        is(Funct3.AND){}
+      }
+    }
+
+    is(OP.LUI){
+      io.out.ctrl.useImm := true.B
+      io.out.ctrl.load := true.B
+    }
+    is(OP.BR){
+      io.out.ctrl.branch := true.B
+      io.out.ctrl.useImm := true.B
+    }
+    is(OP.JALR){
+      io.out.ctrl.jump := true.B
+      io.out.ctrl.useImm := true.B
+    }
+    is(OP.JAL){
+      io.out.ctrl.jump := true.B
+      io.out.ctrl.useImm := true.B
+    }
+  }
 }
--- VStageProcessor/src/main/scala/utilities/InstEnum.scala
+++ VStageProcessor/src/main/scala/utilities/InstEnum.scala
@@ -15,18 +15,18 @@ object OP extends ChiselEnum {
   val JAL = Value("b1101111".U)
 }
 
-object funct3 extends ChiselEnum {
-  val JALR, BEQ, LB, SB, ADDI, ADD, SUB = Value("b000".U)
+object Funct3 extends ChiselEnum {
+  val JALR, BEQ, LB, SB, ADDI, ADDSUB = Value("b000".U)
   val BNE, LH, SH, SLLI, SLL = Value("b001".U)
   val LW, SW, SLTI, SLT = Value("b010".U)
   val SLTIU, SLTU = Value("b011".U)
   val BLE, LBU, XORI, XOR = Value("b100".U)
-  val BGE, LHU, SRLI, SRAI, SRL, SRA = Value("b101".U)
+  val BGE, LHU, SRI, SR = Value("b101".U)
   val BLTU, ORI, OR = Value("b110".U)
   val BGEU, ANDI, AND = Value("b111".U)
 }
 
-object funct7 extends ChiselEnum {
+object Funct7 extends ChiselEnum {
   val SLLI, SRLI, ADD, SLL, SLT, SLTU, XOR, SRL, AND, OR = Value("b0000000".U)
   val SRAI, SUB, SRA = Value("b0100000".U)
 
