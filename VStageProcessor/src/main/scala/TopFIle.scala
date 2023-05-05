6f9563eb774a95fa163a20c32233bfa1439c2297
William <s194050@student.dtu.dk>
2023-05-05 11:12:59 +0200

    verilog synthesis does not work

--- VStageProcessor/src/main/scala/TopFIle.scala
+++
@@ -1,9 +0,0 @@
-import chisel3._
-import ChiselRISC._
-
-import chisel3.stage.ChiselStage
-
-object TopFIle extends App {
-    (new ChiselStage).emitVerilog(new ChiselRISC(false,"InputHex.MEM"), args)
-}
-
