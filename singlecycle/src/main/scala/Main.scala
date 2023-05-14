33628f42e6174f868b64b8ae94746585a228df51
William <s194050@student.dtu.dk>
2023-05-14 19:19:12 +0200

    Remove unused folders

--- singlecycle/src/main/scala/Main.scala
+++
@@ -1,9 +0,0 @@
-import chisel3.emitVerilog
-import components.SingleCycleProcessor
-
-/**
- * An object extending App to generate the Verilog code.
- */
-object Main extends App {
-  emitVerilog(new SingleCycleProcessor())
-}
